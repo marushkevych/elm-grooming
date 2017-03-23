9fe423670940b5b162070a8ec38955507d87ed72
Andrey Marushkevych <marushkevych@gmail.com>
2017-03-22 20:00:38 -0400

    extracting lib styles

--- src/PartialViews/Title.elm
+++ src/PartialViews/Title.elm
@@ -1,11 +1,10 @@
 module PartialViews.Title exposing (root)
 
 import Html exposing (..)
-import Material.Options as Options exposing (css)
-import Material.Typography as Typo
+import Html.Attributes exposing (..)
 import Types exposing (..)
 
 
 root : String -> Html Msg
 root title =
-    Options.styled p [ Typo.display1, Typo.center ] [ text title ]
+    p [ class "title" ] [ text title ]
