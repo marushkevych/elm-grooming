cf902aed13c6a196b3e7b46f644e5ee0469de4ec
Andrey Marushkevych <marushkevych@gmail.com>
2017-04-06 14:38:08 -0400

    Team component

--- src/PartialViews/Team.elm
+++ src/PartialViews/Team.elm
@@ -9,8 +9,18 @@ root : Model -> Html Msg
 root model =
     div [ class "iq-tile iq-tile--gr iq-tile--breadcrumb" ]
         [ span [ class "gr-team" ]
+            (teamName model)
+        ]
+
+
+teamName : Model -> List (Html Msg)
+teamName model =
+    case model.team of
+        Nothing ->
+            []
+
+        Just team ->
             [ text "Team: "
             , strong []
-                [ text "Laurel2" ]
+                [ text team.name ]
             ]
-        ]
